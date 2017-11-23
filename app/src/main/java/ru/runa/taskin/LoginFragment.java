54ce540977dd4ed9be482843cf9dc76e16261608
Russkikh Roman V <rruskih@gmail.com>
2017-11-23 23:51:36 +0300

    LoginActivity

--- app/src/main/java/ru/runa/taskin/LoginFragment.java
+++ app/src/main/java/ru/runa/taskin/LoginFragment.java
@@ -6,12 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by rruskih on 22.11.2017.
  */
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener {
+
+    private TextView _StatusTextView;
 
     @Nullable
     @Override
@@ -19,6 +22,17 @@ public class LoginFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
+        _StatusTextView = view.findViewById(R.id.fragment_login_status);
+
+        view.findViewById(R.id.fragment_login_sign_in_button).setOnClickListener(this);
+        view.findViewById(R.id.fragment_login_sign_out_button).setOnClickListener(this);
+        view.findViewById(R.id.fragment_login_disconnect_button).setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
